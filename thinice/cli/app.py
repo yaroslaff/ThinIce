ab419c74bb4c12536de3ae2e1eb94de2f5e29022
Yaroslav Polyakov <yaroslaff@gmail.com>
2024-11-06 20:43:34 +0700

    display version

--- thinice/cli/app.py
+++ thinice/cli/app.py
@@ -9,12 +9,13 @@ import os
 
 from ..core.credentials import AWSCredentials
 from ..core.vault  import GlacierVault
-
+from ..version import __version__
 
 typerapp = typer.Typer(pretty_exceptions_show_locals=False, 
     # rich_markup_mode="rich"
     no_args_is_help=True,                
     rich_markup_mode="markdown",
+    help=f'ThinIce client for Amazon Glacier ver. {__version__}'
     )
 err_console = Console(stderr=True)
 
@@ -34,9 +35,6 @@ def callback(ctx: typer.Context,
     vault_name: Annotated[str, typer.Option('--vault',envvar='AWS_GLACIER_VAULT', show_default=False, rich_help_panel=panel_env, help='Vault Name')] = None,
     verbose: Annotated[bool, typer.Option('-v', '--verbose', show_default=False, rich_help_panel=panel_env, help='verbose mode')] = False
     ):
-    """
-    Client for Amazon Glacier
-    """
     global vault
     credentials = AWSCredentials(key_id=key_id, secret_key=secret_key, region=region)
     vault = GlacierVault(credentials=credentials, vault_name=vault_name, verbose=verbose, dry = bool(os.getenv('THINICE_DRY')))
